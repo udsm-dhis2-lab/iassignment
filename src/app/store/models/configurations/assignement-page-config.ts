a7e32b5a3c360aa730421f10404f4a5c07639d63
ibrahim Wickama <ibrahimwickama@gmail.com>
2018-11-08 14:27:14 +0300

    update(item-card.component): make ui loaders with icons

--- src/app/store/models/configurations/assignement-page-config.ts
+++ src/app/store/models/configurations/assignement-page-config.ts
@@ -1,21 +1,23 @@
 
 export const assignmentPageConfig: any[] = [
   {id: 'data-assignments', header: 'Data Assignments',
-    summary: 'Make easy assigning of meta-data within DHIS2.', link: '', imageLink: '' },
+    summary: 'Make easy assigning of meta-data within DHIS2.', link: '', imageLink: 'fa-cubes' },
   {id: 'dataSet-group-assignments', header: 'DataSet Groups',
-    summary: 'Providing quick allocating of dataSets into groups.', link: '', imageLink: '' },
+    summary: 'Providing quick allocating of dataSets into groups.', link: '', imageLink: 'fa-cube' },
   {id: 'report-assignments', header: 'Report Access',
-    summary: 'Smart control access to every report of choice to specific user groups.', link: '', imageLink: '' },
-  {id: 'userRole-assignments', header: 'User Roles',
-    summary: 'Easy add and assign roles features to specified user role existing within DHIS2.', link: '', imageLink: '' },
+    summary: 'Smart control access to every report of choice to specific user groups.', link: '', imageLink: 'fa-file-text' },
+  {id: 'map-assignments', header: 'Map Favorites',
+    summary: 'Manages control access on map visualization to users.', link: '', imageLink: 'fa-map' },
   {id: 'chart-assignments', header: 'Charts Access',
-    summary: 'Custom control visualization charts access across users.', link: '', imageLink: '' },
+    summary: 'Custom control visualization charts access across users.', link: '', imageLink: 'fa-pie-chart' },
   {id: 'pivot-assignments', header: 'Pivot Access',
-    summary: 'Manage control visualization tables access across users.', link: '', imageLink: '' },
+    summary: 'Manage control visualization tables access across users.', link: '', imageLink: 'fa-table' },
+  {id: 'userRole-assignments', header: 'User Roles',
+    summary: 'Easy add and assign roles features to specified user role existing within DHIS2.', link: '', imageLink: 'fa-id-card-o' },
   {id: 'user-assignments', header: 'User Accounts',
-    summary: 'Leverages DHIS2 features on user management and assigning accounts.', link: '', imageLink: '' },
+    summary: 'Leverages DHIS2 features on user management and assigning accounts.', link: '', imageLink: 'fa-universal-access' },
   {id: 'resource-assignments', header: 'Resource Control',
-    summary: 'Offers a simple cloud like solution for storing personal files to DHIS2.', link: '', imageLink: '' },
+    summary: 'Offers a simple cloud like solution for storing personal files to DHIS2.', link: '', imageLink: 'fa-archive' },
 ];
 
 // export * from './ass'
